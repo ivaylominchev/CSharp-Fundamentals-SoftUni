460e91fa55a9a26e937b949d02fcec129543e7f8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-09 22:30:24 +0200

    add

---
+++ LabArrays/07.EqualArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr=Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            int differentIndex = 0;
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (firstArr[i] == secondArr[i])
+                {
+                    sum += firstArr[i];
+                }
+                else
+                {
+                    differentIndex = i;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {differentIndex} index");
+                    return;
+                }
+                
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
