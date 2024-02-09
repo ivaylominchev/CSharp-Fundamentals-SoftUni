e3d79a12bb21bc902392f9fa8534e8de148d3c8f
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-09 22:28:30 +0200

    add

---
+++ LabArrays/03.RoundingNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] arr = Console.ReadLine().Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+            int[] roundNumbers = new int[arr.Length];
+
+            for (int i = 0; i <arr.Length ; i++)
+            {
+                roundNumbers[i] = (int)Math
+                    .Round(arr[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{arr[i]} => {roundNumbers[i]}");
+            }
+        }
+    }
+}
