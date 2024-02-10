4e4e4b5a2908e6256dfc09a7358da27002e0a7b2
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-10 20:05:06 +0200

    add

---
+++ ExerciseArrays/05.TopIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool check = true;
+                for (int j = i+1; j <arr.Length; j++)
+                {
+                    if (arr[i] <= arr[j])
+                    {
+                        check = false;
+                        
+                    }
+                    
+                }
+                if (check)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
