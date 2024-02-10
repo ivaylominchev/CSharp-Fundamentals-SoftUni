e5b49465007144f98265954671452c3254fb64d3
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-10 20:02:28 +0200

    add

---
+++ ExerciseArrays/01.Train/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+            int[] arr=new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                total += people;
+                arr[i] = people;
+            }
+            Console.WriteLine(String.Join(" ",arr));
+            Console.WriteLine(total);
+        }
+    }
+}
