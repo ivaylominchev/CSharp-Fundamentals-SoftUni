0513dd2afdc66d86cbfd43dc63d7f7221baf749e
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-10 20:02:54 +0200

    add

---
+++ ExerciseArrays/02.CommonElements/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split();
+            string[] secondArr = Console.ReadLine().Split();
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                for (int j = 0; j < firstArr.Length; j++)
+                {
+                    if (firstArr[j] == secondArr[i])
+                    {
+                        Console.Write(secondArr[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
