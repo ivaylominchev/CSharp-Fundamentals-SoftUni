992976638d24c800944a83141df5bdd4bda1d5ae
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-11 13:52:44 +0200

    add

---
+++ LabMethods/08.MathPower/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(a, x));
+        }
+
+        static double RaiseToPower(double a, int x)
+        {
+            double result = 0d;
+            result = Math.Pow(a, x);
+            return result;
+        }
+    }
+}
