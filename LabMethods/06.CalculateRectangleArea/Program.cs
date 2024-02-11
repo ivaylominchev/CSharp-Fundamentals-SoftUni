b033164342062e2ce602fbcd0ee7e0445a523ab4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-11 13:52:15 +0200

    add

---
+++ LabMethods/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Transactions;
+
+namespace _06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width=double.Parse(Console.ReadLine());
+            double height=double.Parse(Console.ReadLine());
+            double area =GetRectangleArea(width,height);
+            Console.WriteLine(area);
+        }
+        private static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
