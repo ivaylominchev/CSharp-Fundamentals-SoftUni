a7cc5f7569c78a3afa5e2d11b24dc21fea1f6593
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-11 13:53:45 +0200

    add

---
+++ LabMethods/11.MathOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+            switch (command)
+            {
+                case "+":
+                    Add(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    Multiply(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    Subtract(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    Divide(firstNumber, secondNumber);
+                    break;
+            }
+        }
+        static void Add(int first, int second)
+        {
+            int result = first + second;
+            Console.WriteLine(result);
+        }
+        static void Multiply(int first, int second)
+        {
+            int result = first * second;
+            Console.WriteLine(result);
+        }
+        static void Subtract(int first, int second)
+        {
+            int result = first - second;
+            Console.WriteLine(result);
+        }
+        static void Divide(int first, int second)
+        {
+            int result = first / second;
+            Console.WriteLine(result);
+        }
+    }
+}
