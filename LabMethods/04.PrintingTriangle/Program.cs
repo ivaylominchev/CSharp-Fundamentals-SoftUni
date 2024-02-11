6f5ee099c74f9d0e52e39f7d7f4182662eca660b
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-11 13:51:45 +0200

    add

---
+++ LabMethods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            TrianglePrint(number);
+        }
+        
+        private static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <=end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        private static void TrianglePrint(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                PrintLine(1, i);
+            }
+            for (int i = number - 1; i >= 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+    }
+}
