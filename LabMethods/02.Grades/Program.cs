1d605de70bcc8c46d5626296bf41be3d09ec53bb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-11 13:51:12 +0200

    add

---
+++ LabMethods/02.Grades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Transactions;
+
+namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            GradeFinder(grade);
+        }
+
+        private static void GradeFinder(double grade)
+        {
+            if (grade >= 0 && grade < 3)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade < 3.5)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade < 4.5)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade < 5.5)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade <= 6)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
