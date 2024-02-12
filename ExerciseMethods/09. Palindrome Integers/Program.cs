64c926c8efc7eb823cfb0c995956396a9ce80585
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:50:27 +0200

    add

---
+++ ExerciseMethods/09. Palindrome Integers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            while (number != "END")
+            {
+                PalindromeIntegerCheck(number);
+                
+                number = Console.ReadLine();
+            }
+        }
+        static void PalindromeIntegerCheck(string number)
+        {
+            string result ="true";
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != number[number.Length - 1 - i])
+                {
+                    result = "false";
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
