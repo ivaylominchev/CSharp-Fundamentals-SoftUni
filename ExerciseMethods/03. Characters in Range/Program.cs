4ec2ff0d085fb885794c04989a15de4c489c1849
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:48:54 +0200

    add

---
+++ ExerciseMethods/03. Characters in Range/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+
+            CharacterInRange(char1, char2);
+        }
+        static void CharacterInRange(char char1,char char2)
+        {
+            char def = char1;
+            if (char1 > char2)
+            {
+                char1 = char2;
+                char2 = def;
+            }
+            for (int i = char1+1; i < char2; i++) 
+            {
+                char symbol = (char)i;
+                Console.Write(symbol + " ");
+            }
+        }
+
+    }
+}
