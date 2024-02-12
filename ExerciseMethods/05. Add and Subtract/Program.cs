57d8b5004c1299555b090c009db8f4137d1e7eca
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:49:21 +0200

    add

---
+++ ExerciseMethods/05. Add and Subtract/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(Subtract(a,b,c));
+            
+        }
+
+        static int SumOfIntegers(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+       static int Subtract(int a, int b, int c) 
+        {
+            int result = SumOfIntegers(a, b) - c;
+            return result; 
+        }
+
+    }
+}
