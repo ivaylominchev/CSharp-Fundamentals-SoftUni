da33f025644f4e2557a86ef3f917ead502fb13bb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:50:10 +0200

    add

---
+++ ExerciseMethods/08. Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            FactorialDivision(a, b);
+        }
+        static void FactorialDivision(double a, double b)
+        {
+            double firstFactoriel = 1;
+            double secondFactoriel = 1;
+            double result = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                firstFactoriel *= i;
+            }
+            for (int i = 1; i <= b; i++)
+            {
+                secondFactoriel *= i;
+            }
+            result = firstFactoriel / secondFactoriel;
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
