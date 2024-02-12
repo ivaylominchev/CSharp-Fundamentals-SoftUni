1dd15f839c9fecf29ef2db2325bf2d965c8c0df7
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:48:40 +0200

    add

---
+++ ExerciseMethods/02. Vowels Count/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            int vowelCount = VowelCount(sentence);
+
+            Console.WriteLine(vowelCount);
+        }
+        static int VowelCount(string sentence)
+        {
+            int count = 0;
+            sentence = sentence.ToLower();
+            
+            foreach (var symbol in sentence)
+            {
+                if (symbol == 'e'
+                    || symbol == 'a'
+                    || symbol == 'o'
+                    || symbol == 'i'
+                    || symbol == 'u')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
