5e67c26b2f72f686cf8271927d33f26a1c8ec24a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-12 23:49:55 +0200

    add

---
+++ ExerciseMethods/07. NxN Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+
+        static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine(n + " ");
+            }
+        }
+    }
+}
+
+/*namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Matrix(n);
+        }
+
+        static void Matrix(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}*/
