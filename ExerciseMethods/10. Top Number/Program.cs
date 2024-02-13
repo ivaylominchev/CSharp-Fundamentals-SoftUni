15b5e11bdb03eca736ce135a15587ec4a2a4fe97
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-13 23:13:57 +0200

    add

---
+++ ExerciseMethods/10. Top Number/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            TopNumber(n);
+        }
+        static void TopNumber(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string s = i.ToString();
+                bool oddNumber = false;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    sum += s[j];
+                    if (s[j] % 2 != 0)
+                    {
+                        oddNumber = true;
+                    }
+
+                }
+                if (sum % 8 == 0)
+                {
+                    if (oddNumber)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+                sum = 0;
+            }
+        }
+    }
+}
