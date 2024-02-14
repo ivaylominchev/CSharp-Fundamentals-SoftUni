e8c99e1faef39ad03a694c0ddfd4b48174059282
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-14 22:39:14 +0200

    add

---
+++ ExerciseLists/02.ChangeList/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string input;
+            while ((input=Console.ReadLine())!="end") 
+            {
+                string[] command = input.Split();
+                if (command[0] == "Delete")
+                {
+                    int number = int.Parse(command[1]);
+                    list.RemoveAll(x => x == number);
+                }
+                else if (command[0] == "Insert")
+                {
+                    int element = int.Parse(command[1]);
+                    int index = int.Parse(command[2]);
+                    list.Insert(index, element);
+                }           
+
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
