f78b18614f1a918e29ddb7ea5aab2560bc6e72aa
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-14 22:39:34 +0200

    add

---
+++ ExerciseLists/03.HouseParty/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guestList=int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < guestList; i++)
+            {
+                string[] arguments =Console.ReadLine().Split();
+                string name = arguments[0];
+                if (arguments[2]=="going!")
+                {
+                    if (list.Exists(e => e == name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        list.Add(name);
+                    }
+                }
+                else if (arguments[2] == "not")
+                {
+                    if (list.Exists(x => x == name))
+                    {
+                        list.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+               
+            }
+            Console.WriteLine(string.Join("\n",list));
+        }
+    }
+}
