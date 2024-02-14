b8e696cf5d8ca706bdad836ce705cf2cfae3d962
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-14 22:37:34 +0200

    add

---
+++ ExerciseLists/01.Train/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons=Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxcapacity=int.Parse(Console.ReadLine());
+
+            string input;
+            while ((input=Console.ReadLine())!="end") 
+            {
+                string[] commands = input.Split();
+
+                if (commands[0]=="Add")
+                {
+                    wagons.Add(int.Parse(commands[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(commands[0]);
+                    for (int i = 0; i < wagons.Count; i++) 
+                    {
+                        if (passengers +wagons[i]<= maxcapacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
