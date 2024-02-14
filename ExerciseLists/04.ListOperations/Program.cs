b9ea4230853a8dab7e08c8076d24e50f1d2f37f9
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-14 22:39:47 +0200

    add

---
+++ ExerciseLists/04.ListOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.ListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list =Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();  
+        }
+    }
+}
