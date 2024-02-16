1a4d58d387f280f946752738ac8a0a38718e7c10
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-16 22:40:24 +0200

    new add

---
+++ LabLists/03. Merging Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> rezult = new List<int>();
+
+            int minCount = Math.Min(firstList.Count, secondList.Count);
+
+            for (int i = 0; i < minCount; i++)
+            {
+                rezult.Add(firstList[i]);
+                rezult.Add(secondList[i]);
+            }
+
+            if (minCount == firstList.Count)
+            {
+                for (int i = minCount; i < secondList.Count; i++)
+                {
+                    rezult.Add(secondList[i]);
+                }
+            }
+            else
+            {
+                for (int i = minCount; i < firstList.Count; i++)
+                {
+                    rezult.Add(firstList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", rezult));
+        }
+    }
+}
