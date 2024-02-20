eb1d1d3d34cdd75f3fc2541f1f191b9bd3651fa4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-20 22:17:25 +0200

    new add

---
+++ LabAssociativeArrays/03. Word Synonyms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words= new Dictionary<string, List<string>>(); 
+            
+            for (int i = 0; i < n; i++) 
+            { 
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    List<string> newWords = new List<string>();
+
+                    words.Add(word,newWords);
+
+                }
+
+                List<string> newList = words[word];
+                newList.Add(synonym);
+
+            }
+            
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ",word.Value)}");
+            }
+        }
+    }
+}
