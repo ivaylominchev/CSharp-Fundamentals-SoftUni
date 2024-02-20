baf9899ae57acfdf5a8b42fc99decfedaa77243d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-20 22:16:37 +0200

    new add

---
+++ LabAssociativeArrays/01. Count Real Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers=Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            SortedDictionary<int,int> sortedNumbers = new SortedDictionary<int,int>();
+
+            foreach (int number in numbers)
+            {
+                if (!sortedNumbers.ContainsKey(number))
+                {
+                    sortedNumbers.Add(number, 0);
+                }
+
+                sortedNumbers[number]++;
+            }
+
+            foreach (var number in sortedNumbers)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
