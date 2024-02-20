8cf749be8f5b74723bd8fdd084d424de6e769e7d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-20 22:17:04 +0200

    new add

---
+++ LabAssociativeArrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordToLower=word.ToLower();
+                if (!counts.ContainsKey(wordToLower))
+                {
+                    counts.Add(wordToLower, 0);
+                }
+                counts[wordToLower]++;
+            }
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 !=0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
