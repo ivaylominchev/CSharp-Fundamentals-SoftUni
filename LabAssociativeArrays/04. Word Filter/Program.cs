d37cc59b785c2b6b06ec840d87898f379ff34601
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-20 22:17:38 +0200

    new add

---
+++ LabAssociativeArrays/04. Word Filter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split()
+                .Where(x => x.Length % 2 == 0)
+                .ToList();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
+//2 nachin
+/*Console.ReadLine()
+    .Split()
+    .Where(x => x.Length % 2 == 0)
+    .ToList()
+    .ForEach(Console.WriteLine);*/
