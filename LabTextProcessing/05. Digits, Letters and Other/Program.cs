5ed9bc927a2560516bc382f45898c8753dda7130
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-21 21:18:19 +0200

    new add

---
+++ LabTextProcessing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(string.Join("",input.Where(char.IsDigit)));
+            Console.WriteLine(string.Join("", input.Where(char.IsLetter)));
+            Console.WriteLine(string.Join("", input.Where(x => !char.IsDigit(x) && ! char.IsLetter(x))));
+        }
+    }
+}
