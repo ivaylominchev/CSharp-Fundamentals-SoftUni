72e4595b0fc3f52db408164cdb6a563dea4eedcf
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-21 21:17:15 +0200

    new add

---
+++ LabTextProcessing/01. Reverse Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "end")
+            {
+                char[] reversedWord=word.Reverse().ToArray();
+                Console.WriteLine($"{word} = {string.Join("",reversedWord)}");
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
