93467399c9ae6600d1799ba02ae446a84d61003d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-21 21:18:33 +0200

    new add

---
+++ LabTextProcessing/05. Digits,LettersandOther/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Channels;
+
+namespace _05._Digits_LettersandOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letter = string.Empty;
+            string symbols = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (char.IsDigit(c))
+                {
+                    digits += c;  
+                }
+                else if (char.IsLetter(c))
+                {
+                    letter += c;
+                }
+                else
+                {
+                    symbols += c;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letter);
+            Console.WriteLine(symbols);
+
+        }
+    }
+}
