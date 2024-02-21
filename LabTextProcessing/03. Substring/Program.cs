f8b6427d78a4027d79f0b2839c54b65665ef749d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-21 21:17:50 +0200

    new add

---
+++ LabTextProcessing/03. Substring/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove=Console.ReadLine();
+            string text=Console.ReadLine();
+
+            while (text.Contains(wordToRemove))
+            {
+                int index = text.IndexOf(wordToRemove);
+                text = text.Remove(index, wordToRemove.Length);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
