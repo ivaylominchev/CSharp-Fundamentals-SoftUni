df899e715bf3005a59a9afe37e331d9850188bad
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-25 23:28:43 +0200

    new

---
+++ ExerciseTextProcessing/03. Extract File 2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _03._Extract_File_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine($"File name: {Path.GetFileNameWithoutExtension(filePath)}");
+            Console.WriteLine($"File extension: {Path.GetExtension(filePath).Replace(".","")}");
+        }
+    }
+}
