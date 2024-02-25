c1e4b6f181162e0333ad54f44923d5fbfa69d8e4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-25 23:13:03 +0200

    new add

---
+++ ExerciseTextProcessing/03. Extract File/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            string fileName = string.Empty;
+            string extension = string.Empty;
+
+            int fileNameIndex = path.LastIndexOf("\\");
+            int extensionIndex = path.LastIndexOf(".");
+
+            fileName = path.Substring(fileNameIndex + 1,extensionIndex-fileNameIndex - 1);
+            extension = path.Substring(extensionIndex + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
