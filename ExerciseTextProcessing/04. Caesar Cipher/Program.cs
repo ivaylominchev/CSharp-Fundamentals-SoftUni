8b5d9d6bf5f4c88becb19e5b9c2f256347d80e63
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-26 20:17:03 +0200

    new add

---
+++ ExerciseTextProcessing/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder encrypted = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char original = text[i];
+                encrypted.Append((char)(original + 3));
+            }
+
+            Console.WriteLine(encrypted);
+        }
+    }
+}
