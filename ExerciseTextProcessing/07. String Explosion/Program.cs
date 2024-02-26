c0ead84bed38a2b34e5b1f8c61de53c96491221d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-26 20:40:19 +0200

    new add

---
+++ ExerciseTextProcessing/07. String Explosion/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = ProcessExplosions(input);
+            Console.WriteLine(result);
+        }
+
+        private static string ProcessExplosions(string? input)
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    resultBuilder.Append(input[i]);
+                }
+                else if (strength == 0)
+                {
+                    resultBuilder.Append(input[i]);
+                }
+                else
+                {
+                    strength--;
+                }
+            }
+
+            return resultBuilder.ToString();
+        }
+    }
+}
