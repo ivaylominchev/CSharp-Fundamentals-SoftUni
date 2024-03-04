ff420faa550efadc6b7b81c4aa2caaa8a830c43c
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-05 00:54:32 +0200

    new add

---
+++ ExerciseLists/07. Append Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Security;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> array = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> newArray = new List<string>();
+            
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                List<string> symbols = array[i]
+                    .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                newArray.AddRange(symbols);
+            }
+
+            Console.WriteLine(string.Join(" ", newArray));
+        }
+    }
+}
