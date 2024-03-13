1b7e8a098ccf4a8e64ff214bc0baa307c0e1211a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-13 16:09:43 +0200

    finish task

--- LabObjectsAndClasses/02. Big Factorial/Program.cs
+++ LabObjectsAndClasses/02. Big Factorial/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _02._Big_Factorial
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
         }
     }
 }
