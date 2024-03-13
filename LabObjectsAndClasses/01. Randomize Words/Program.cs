6f0c6894967f2163d0c56d879c5bcde7b3f572da
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-13 15:50:19 +0200

    create project

---
+++ LabObjectsAndClasses/01. Randomize Words/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
