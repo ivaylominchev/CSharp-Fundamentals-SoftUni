9ce372801ecca7dabf6dfd5c4a1cdcfcdb3eacc8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-13 16:03:56 +0200

    Update Program.cs

--- LabObjectsAndClasses/01. Randomize Words/Program.cs
+++ LabObjectsAndClasses/01. Randomize Words/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = Console.ReadLine().Split();
+            Random rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(words.Length);
+                string temp = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = temp;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
         }
     }
 }
