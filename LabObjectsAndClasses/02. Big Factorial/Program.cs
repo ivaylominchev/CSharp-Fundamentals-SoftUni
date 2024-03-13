146a6d7c05467cf73c78451128c08e1a3e80d1fc
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-13 16:04:24 +0200

    create task

---
+++ LabObjectsAndClasses/02. Big Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
